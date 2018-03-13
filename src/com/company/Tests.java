d3979d077d92d84d80edd6f1e0dabc30eb1c41eb
Ilia Voronin <iliavoronin9898@gmail.com>
2018-03-13 16:40:07 +0300

    update
    добавлены новые тесты, старые переделаны

--- src/com/company/Tests.java
+++ src/com/company/Tests.java
@@ -5,55 +5,84 @@ import static org.junit.Assert.assertEquals;
 
 public class Tests {
 
-    BinaryTree testTree = new BinaryTree();
+    private BinaryTree testTree = new BinaryTree();
 
     @Test
     public void addNode() {
-        testTree.addNode(5,"root");
-        testTree.addNode(3,"leftChild");
-        testTree.addNode(7,"rightChild");
-        assertEquals("root 5", testTree.root.toString());
-        assertEquals("leftChild 3", testTree.root.leftChild.toString());
-        assertEquals("rightChild 7", testTree.root.rightChild.toString());
+        testTree.addNode(5);
+        testTree.addNode(3);
+        testTree.addNode(7);
+        assertEquals("Value: 5", testTree.root.toString());
+        assertEquals("Value: 3", testTree.root.leftChild.toString());
+        assertEquals("Value: 7", testTree.root.rightChild.toString());
     }
 
     @Test
     public void contains() {
-        testTree.addNode(1,"testName");
+        testTree.addNode(1);
         assertEquals(true, testTree.contains(1));
         assertEquals(false, testTree.contains(999));
     }
 
     @Test
     public void findNode () {
-        Node newNode = new Node(1,"testName");
+        Node newNode = new Node(1);
         testTree.addNode(newNode);
-        assertEquals("testName 1", testTree.findNode(1).toString());
+        assertEquals("Value: 1", testTree.findNode(1).toString());
     }
 
     @Test
     public void deleteNode() {
-        testTree.addNode(10,"a");
-        testTree.addNode(5,"b");
-        testTree.addNode(15,"c");
+        testTree.addNode(10);
+        testTree.addNode(5);
+        testTree.addNode(15);
         testTree.deleteNode(5);
         testTree.deleteNode(15);
         assertEquals(null,testTree.root.leftChild);
-        assertEquals(null,testTree.root.rightChild);                     //удаление "листков"
+        assertEquals(null,testTree.root.rightChild);                         //удаление "листков"
 
-        testTree.addNode(15,"b");
-        testTree.addNode(20,"c");
+        testTree.addNode(15);
+        testTree.addNode(20);
         testTree.deleteNode(15);
-        assertEquals("c 20", testTree.root.rightChild.toString());      //удаление элемента с правым ребенком
+        assertEquals("Value: 20", testTree.root.rightChild.toString());      //удаление элемента с правым ребенком
 
-        testTree.addNode(13, "d");
+        testTree.addNode(13);
         testTree.deleteNode(20);
-        assertEquals("d 13", testTree.root.rightChild.toString());      //удаление элемнта с левым ребенком
+        assertEquals("Value: 13", testTree.root.rightChild.toString());      //удаление элемнта с левым ребенком
 
-        testTree.addNode(11,"e");
-        testTree.addNode(14,"f");
+        testTree.addNode(11);
+        testTree.addNode(14);
         testTree.deleteNode(13);
-        assertEquals("f 14", testTree.root.rightChild.toString());      //удаление элемента с двумя детьми
-        assertEquals("e 11", testTree.findNode(14).leftChild.toString());
+        assertEquals("Value: 14", testTree.root.rightChild.toString());      //удаление элемента с двумя детьми
+        assertEquals("Value: 11", testTree.findNode(14).leftChild.toString());
+
     }
+
+    @Test
+    public void tooString() {
+        testTree.addNode(50);
+        testTree.addNode(30);
+        testTree.addNode(10);
+        testTree.addNode(40);
+        testTree.addNode(5);
+        testTree.addNode(15);
+        testTree.addNode(35);
+        testTree.addNode(45);
+        assertEquals("5 10 15 30 35 40 45 50", testTree.toString());
+    }
+
+    @Test
+    public void equals() {
+        BinaryTree a = new BinaryTree();
+        BinaryTree b = new BinaryTree();
+        a.addNode(2);
+        a.addNode(3);
+        b.addNode(2);
+        b.addNode(3);
+        assertEquals(true, a.equals(b));
+        b.addNode(1);
+        assertEquals(false, a.equals(b));
+
+    }
+
 }
